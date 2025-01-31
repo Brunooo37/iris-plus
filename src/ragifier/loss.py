c9917a9dede8a0827ddb482a45cd395227dc5dca
Elliot Hill <elliot.douglas.hill@gmail.com>
2025-01-30 23:44:57 -0500

    Update loss argument names

--- src/ragifier/loss.py
+++ src/ragifier/loss.py
@@ -10,8 +10,8 @@ import torch.nn.functional as F
 
 
 # encourages queries to be close to each other
-def query_penalty(queries):
-    return torch.cdist(queries, queries, p=2).mean()
+def query_penalty(query):
+    return torch.cdist(query, query, p=2).mean()
 
 
 # encourages retrieved vectors to be close to the query
@@ -19,11 +19,11 @@ def vector_penalty(query, vectors):
     return torch.cdist(vectors, query, p=2).mean()
 
 
-def loss(outputs, labels, queries, vectors, alpha, beta):
+def loss(outputs, labels, query, vectors, alpha, beta):
     # queries = F.normalize(queries, p=2, dim=-1)
     # vectors = F.normalize(vectors, p=2, dim=-1)
     return (
         F.cross_entropy(outputs, labels)
-        + alpha * query_penalty(queries)
-        + beta * vector_penalty(queries, vectors)
+        + alpha * query_penalty(query)
+        + beta * vector_penalty(query, vectors)
     )
